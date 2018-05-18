6f0fd8f4afc344691c17a415240c76dd278c8d1d
WhiskeyRomeoLima <wrlucas@it-pma.com>
2018-05-18 16:52:37 -0500

    xxx

--- notes2-edit.js
+++ notes2-edit.js
@@ -3,7 +3,7 @@
 const noteId = location.hash.substring(1);
 const notes = getSavedNotes();
 const note = notes.find((note) => note.id === note.Id);
-
+debugger
 if (note === undefined) {
     location.assign('notes2.html');
 }
